4bb51e79898b33cbcdef2165382c9369cd07786e
Olli Rouvinen <olli.rouvinen@gmail.com>
2017-03-16 22:37:35 +0200

    /api/user POST endpoint for user account creation

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ var pg      = require('pg');
 var async   = require('async');
 var path    = require('path');
 var url     = require('url');
+var bodyParser = require('body-parser');
 
 var app = express();
 
@@ -38,6 +39,33 @@ const db = new pg.Pool(dbConfig);
 // Frontend build and static assets from under public/
 app.use(express.static('public'));
 
+var jsonParser = bodyParser.json();
+
+/*
+ * Create a user
+ * Params (in request body):
+ *  "name": username
+ *  "email": email address
+ *  "password": password
+ */
+app.post('/api/user', jsonParser, (req, res) => {
+  if (isEmpty(req.body))
+    return res.status(400).json({ error: "Missing request body" });
+
+  const { name, email, password } = req.body;
+
+  db.query("INSERT INTO users(name, email, password) VALUES ($1, $2, $3)",
+    [name, email, password])
+    .then(result => {
+      res.status(200);
+      res.json({ success: "true" });
+    }).catch(error => {
+      res.status(409);
+      res.json({ error: error.detail });
+    });
+});
+
+
 // Rest of the urls are for front-end
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'public/index.html'));
@@ -48,3 +76,5 @@ app.listen(serverOptions.port, () => {
   console.log('listening on port %s', serverOptions.port);
 });
 
+
+const isEmpty = obj => Object.keys(obj).length === 0;
