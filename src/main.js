bfb48a030df878c5520c6a5121b5d46e3270de33
Olli Rouvinen <olli.rouvinen@gmail.com>
2017-04-06 08:06:17 +0300

    Don't to try to handle protected routes with invalid token

--- src/main.js
+++ src/main.js
@@ -49,7 +49,6 @@ var jsonParser = bodyParser.json();
 const tokenChecker = (err, req, res, next) => {
   if (err.name === 'UnauthorizedError')
     res.status(401).json({ error: "Invalid auth token "});
-  next();
 };
 
 // Routes
