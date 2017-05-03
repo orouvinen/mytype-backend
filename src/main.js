eae7abba7b8d31948138e836dda5ba0ce124176a
Olli Rouvinen <olli.rouvinen@gmail.com>
2017-05-03 23:35:29 +0300

    Use tokenChecker for POST /api/users/:id/results

--- src/main.js
+++ src/main.js
@@ -68,7 +68,7 @@ app.delete('/api/users/:id', jwt({ secret: auth.secret }), tokenChecker, jsonPar
 app.post('/api/competitions', jwt({ secret: auth.secret }), tokenChecker, jsonParser, competition.createCompetition);
 app.get('/api/competitions/:id', jsonParser, competition.getCompetition);
 app.get('/api/users/:id/results', jwt({ secret: auth.secret }), jsonParser, user.getUserResults);
-app.post('/api/users/:id/results/', jwt({ secret: auth.secret }), jsonParser, user.saveResult);
+app.post('/api/users/:id/results/', jwt({ secret: auth.secret }), tokenChecker, jsonParser, user.saveResult);
 
 
 // Rest of the urls are for front-end
