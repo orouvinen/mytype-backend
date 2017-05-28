dc0a464ad5f7032739cce64b74eadb817f05ee5d
Olli Rouvinen <olli.rouvinen@gmail.com>
2017-05-28 21:13:26 +0300

    Extend database.
    - Add fields for WPM & accuracy aggregate values for users
    - Add table stats for storing global stats (wpm & acc atm.)
    - Add registered date to user records

--- db/create-mytype-db.sql
+++ db/create-mytype-db.sql
@@ -1,6 +1,7 @@
 DROP TABLE IF EXISTS results;
 DROP TABLE IF EXISTS competitions;
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS stats;
 
 CREATE TABLE users(
   id SERIAL,
@@ -8,7 +9,13 @@ CREATE TABLE users(
   email varchar(64),
   password CHAR(128),
   salt CHAR(20),
-  admin BOOLEAN DEFAULT FALSE
+  admin BOOLEAN DEFAULT FALSE,
+  registered TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+
+  -- aggregate values updated on fly
+  avg_wpm REAL DEFAULT 0.0,
+  avg_acc REAL DEFAULT 0.0,
+  num_typing_tests INTEGER DEFAULT 0
 );
 ALTER TABLE users ADD PRIMARY KEY(id);
 ALTER TABLE users ADD UNIQUE(email);
@@ -19,7 +26,7 @@ CREATE TABLE competitions(
   finished BOOLEAN DEFAULT FALSE,
   content TEXT,
   language CHAR(3),
-  duration INTEGER DEFAULT 24
+  duration INTEGER DEFAULT 24,
 );
 ALTER TABLE competitions ADD PRIMARY KEY(id);
 
@@ -34,3 +41,8 @@ CREATE TABLE results (
 ALTER TABLE results ADD PRIMARY KEY(usr, start_time);
 ALTER TABLE results ADD FOREIGN KEY(competition) REFERENCES competitions(id) ON DELETE CASCADE;
 ALTER TABLE results ADD FOREIGN KEY(usr) REFERENCES users(id) ON DELETE CASCADE;
+
+CREATE TABLE stats(
+  avg_wpm REAL DEFAULT 0.0, -- avg wpm for all typed typing tests
+  avg_acc REAL DEFAULT 0.0
+);
